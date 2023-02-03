067ba14d3fdfc0d6cdd76d0fe784d7fbbd613c7d
Mouh-Cine-coder <mouhcinedafiaa@gmail.com>
2023-02-03 17:06:47 +0100

    change to class-based views for home and about pages

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import homePage
+from .views import HomePageView, AboutPageView
 
 urlpatterns = [
-    path("", homePage, name="home-page")
+    path("about/", AboutPageView.as_view(), name="about-page"),
+    path("", HomePageView.as_view(), name="home-page"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 # Create your views here.
 
-def homePage(request):
-    return HttpResponse('<h1>Hello world</h1>')
+class HomePageView(TemplateView):
+    template_name = "home.html"
+    
+class AboutPageView(TemplateView):
+    template_name = "about.html"
